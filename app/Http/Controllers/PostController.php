08fc447f240be153618cbd9b9b927b4356987d1a
AhmadMdi <admimohammadi@gmail.com>
2023-12-12 00:34:13 +0330

    change PostController
    -set route model binding for show method => call data in blade file
    -set compact method for call variables
    -request categoryItems for insert post
    -set query by ElequantORM for slug edit
    -set complate crud Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,30 +3,61 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\NewPostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function show($slug)
+    public function show(Post $post)
     {
-        $post = Post::query()->where('slug',$slug)->firstOrFail();
-        return view('posts.post',[
-            'post' => $post
-        ]);
+        return view('posts.show', compact('post'));
     }
 
     public function create()
     {
-        return view('posts.create');
+        $categories = Category::all();
+        return view('posts.create',compact('categories'));
     }
+
     public function store(NewPostRequest $request)
     {
         Post::query()->create([
-           'title' => $request->title,
-           'slug' => $request->slug,
-           'body' => $request->body,
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'body' => $request->body,
+            'category_id' => $request->category_id,
         ]);
         return redirect('/post/create');
+    }
+
+    public function edit(Post $post)
+    {
+        $categories = Category::all();
+        return view('posts.edit', compact('post','categories'));
+    }
+
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        $slugExists = Post::query()->where('slug', $request->slug)
+            ->where('id', '!=', $post->id)
+            ->exists();
+        if ($slugExists) {
+            return redirect()->back()->withErrors(['slug' => 'the slug already been taken']);
+        }
+        $post->update([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'body' => $request->body,
+            'category_id' => $request->category_id,
+        ]);
+        return redirect('/');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
