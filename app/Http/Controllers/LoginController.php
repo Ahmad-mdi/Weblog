721731d8dd48a50282453f93d9b721b8f9dcdbc9
AhmadMdi <admimohammadi@gmail.com>
2023-12-12 00:28:54 +0330

    create LoginController
    login busines by mobile number and password record

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function create()
+    {
+        return view('authentication.login');
+    }
+
+    public function store(LoginRequest $request)
+    {
+        $user = User::query()->where('phone',$request->phone)->firstOrFail();
+        if (!Hash::check($request->get('password'),$user->password)) {
+            return redirect()->back()->withErrors(['password'=>'password is not correct']);
+        }
+        auth()->login($user);
+        return redirect('/');
+    }
+
+    public function destroy()
+    {
+        auth()->logout();
+        return redirect('/');
+    }
+}
