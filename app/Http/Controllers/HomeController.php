a047ed9077918a7b2c9e34628be292b0a1d6a733
AhmadMdi <admimohammadi@gmail.com>
2023-12-11 23:52:41 +0330

    set compact method for send variables

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,8 +12,7 @@ class HomeController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return view('welcome',[
-            'posts' => $posts,
-        ]);
+        $categories = Category::all();
+        return view('welcome',compact('posts','categories'));
     }
 }
