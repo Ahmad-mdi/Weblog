d634075491040fb50361d56f7978b425e9c0423d
AhmadMdi <admimohammadi@gmail.com>
2023-12-13 15:44:05 +0330

    set aut and geust middleware in roter app

--- routes/web.php
+++ routes/web.php
@@ -4,39 +4,50 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\SignupController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-//home:
-Route::get('/',[HomeController::class,'index']);
-//posts:
-Route::get('/post/create',[PostController::class,'create']);
-Route::post('/post/store',[PostController::class,'store']);
-Route::get('/post/{post}/edit',[PostController::class,'edit']); //route model binding
-Route::put('/post/{post}',[PostController::class,'update']); //route model binding
-Route::get('/post/{post}',[PostController::class,'show']); //route model binding
-Route::delete('/post/{post}/delete',[PostController::class,'destroy']); //route model binding
-//category:
-Route::get('/category',[CategoryController::class,'index']);
-Route::get('/category/create',[CategoryController::class,'create']);
-Route::post('/category/store',[CategoryController::class,'store']);
-Route::get('/category/{category}/edit',[CategoryController::class,'edit']);
-Route::put('/category/{category}',[CategoryController::class,'update']);
-Route::get('/category/{category}',[CategoryController::class,'show']);
-
+Route::get('/', [HomeController::class, 'index']);
 //authentication:
-Route::get('/login/create',[LoginController::class,'create']);
-Route::post('/login/store',[LoginController::class,'store']);
-Route::delete('/logout',[LoginController::class,'destroy']);
-Route::get('/signup/create',[SignupController::class,'create']);
-Route::post('/signup/store',[SignupController::class,'store']);
+Route::middleware('guest')->group(function () {
+    Route::get('/login/create', [LoginController::class, 'create'])->name('login');
+    Route::post('/login/store', [LoginController::class, 'store']);
+    Route::get('/signup/create', [SignupController::class, 'create']);
+    Route::post('/signup/store', [SignupController::class, 'store']);
+});
+Route::delete('/logout', [LoginController::class, 'destroy']);
+//*****************************************************************************************************************************
+Route::middleware('auth')->group(function () {
+    Route::get('/post/create', [PostController::class, 'create']);
+    Route::post('/post/store', [PostController::class, 'store']);
+    Route::get('/post/{post}/edit', [PostController::class, 'edit']);
+    Route::put('/post/{post}', [PostController::class, 'update']);
+    Route::delete('/post/{post}/delete', [PostController::class, 'destroy']);
+
+    Route::get('/roles', [RoleController::class, 'index']);
+    Route::get('/roles/create', [RoleController::class, 'create']);
+    Route::post('/roles/store', [RoleController::class, 'store']);
+    Route::get('/roles/{role}/edit', [RoleController::class, 'edit']);
+    Route::put('/roles/{role}', [RoleController::class, 'update']);
+    Route::delete('/roles/{role}', [RoleController::class, 'destroy']);
+
+    Route::get('/profile', [ProfileController::class, 'show']);
+    Route::get('/profile/edit', [ProfileController::class, 'edit']);
+    Route::put('/profile/update', [ProfileController::class, 'update']);
+
+    Route::get('/category', [CategoryController::class, 'index']);
+    Route::get('/category/create', [CategoryController::class, 'create']);
+    Route::post('/category/store', [CategoryController::class, 'store']);
+    Route::get('/category/{category}/edit', [CategoryController::class, 'edit']);
+    Route::put('/category/{category}', [CategoryController::class, 'update']);
+    Route::get('/category/{category}', [CategoryController::class, 'show']);
+
+    Route::resource('users',UserController::class);
+});
+
+Route::get('/post/{post}', [PostController::class, 'show']);
+
+
