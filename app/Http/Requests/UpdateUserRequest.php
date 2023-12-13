551bc20418e0dc075436186bf800aebfc98f4257
AhmadMdi <admimohammadi@gmail.com>
2023-12-13 14:54:53 +0330

    validation for updating users

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'role_id' => 'required|exists:roles,id',
+            'name' => 'required|string|',
+            'phone' => 'required|min:11|max:11',
+            'email' => 'email|required',
+        ];
+    }
+}
